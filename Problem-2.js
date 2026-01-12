6dc09b4465d4818d836a786e97ba93f713ae80a4
minhazahmmed <minhazahmmed231@gmail.com>
2026-01-13 00:14:05 +0600

    problem 2 done

---
+++ Problem-2.js
@@ -0,0 +1,13 @@
+function countVowels(str){
+  let count = 0;
+  for(let i = 0;i<str.length; i++)
+  {
+    if(str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U'||str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u') {
+     count++;
+    }
+  }
+  return count;
+
+}
+console.log(countVowels('apple'))
+
