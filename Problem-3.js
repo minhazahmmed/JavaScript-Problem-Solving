37ea8fd8c789383c35638c45f03e913dcaa1873c
minhazahmmed <minhazahmmed231@gmail.com>
2026-01-13 06:46:41 +0600

    done

--- Problem-3.js
+++ Problem-3.js
@@ -1,18 +1,17 @@
-function Palindrome(str){
-    let Forward = '';
-    let Backward = '';
-    for(let i = 0; i<str.length;i++){
-        Forward+=str[i];
-    }
-    for(let j = str.length-1;j>=0;j--){
-        Backward += str[j];
-    }
-    if(Forward === Backward){
-        return true;
-    }
-    else{
-        return false;
-    }
+function Palindrome(str) {
+  let Forward = "";
+  let Backward = "";
+  for (let i = 0; i < str.length; i++) {
+    Forward += str[i];
+  }
+  for (let j = str.length - 1; j >= 0; j--) {
+    Backward += str[j];
+  }
+  if (Forward === Backward) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-console.log(Palindrome('madam'))
+console.log(Palindrome("madam"));
